03d25682881495e07c1efce107e0871796a66fb7
Thomas Glatzer <glatzert@outlook.de>
2020-06-17 17:46:24 +0200

    Minimal issuer implementation

---
+++ src/ACME.CertProvider.ACDS/CertificateIssuer.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using TGIT.ACME.Protocol.Services;
+using CertEnroll = CERTENROLLLib;
+
+namespace TGIT.ACME.CertProvider.ACDS
+{
+    public sealed class CertificateIssuer : ICertificateIssuer
+    {
+        public Task<byte[]> IssueCertificate(string csr, CancellationToken cancellationToken)
+        {
+            var request = new CertEnroll.CX509CertificateRequestPkcs10Class();
+            request.InitializeDecode(csr, CertEnroll.EncodingType.XCN_CRYPT_STRING_BASE64_ANY);
+
+            var enrollment = new CertEnroll.CX509EnrollmentClass();
+            enrollment.InitializeFromRequest(request);
+            enrollment.Enroll();
+            var certificate = enrollment.Certificate[CertEnroll.EncodingType.XCN_CRYPT_STRING_BASE64_ANY];
+
+            return new byte[];
+        }
+    }
+}
