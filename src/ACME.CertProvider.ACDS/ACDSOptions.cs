e937c78ec7ad4650bf2b4d96f61068e1d7014353
Thomas Glatzer <glatzert@outlook.de>
2020-06-23 18:25:48 +0200

    Remove the unfinished provider

--- src/ACME.CertProvider.ACDS/ACDSOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TGIT.ACME.CertProvider.ACDS
-{
-    public class ACDSOptions
-    {
-        public string CAServer { get; set; }
-        public string? CAAttributes { get; set; }
-    }
-}
