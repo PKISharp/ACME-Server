9f0fb038ec297df6585eda3319653d335c9b5947
Thomas Glatzer <glatzert@outlook.de>
2020-06-27 13:20:32 +0200

    Prepare NuGet Packaging

--- src/ACME.Protocol/Services/Abstractions/ICertificateIssuer.cs
+++ src/ACME.Protocol/Services/Abstractions/ICertificateIssuer.cs
@@ -4,11 +4,6 @@ using TGIT.ACME.Protocol.Model;
 
 namespace TGIT.ACME.Protocol.Services
 {
-    public interface ICsrValidator
-    {
-        Task<(bool isValid, AcmeError? error)> ValidateCsrAsync(Order order, string csr, CancellationToken cancellationToken);
-    }
-
     public interface ICertificateIssuer
     {
         Task<(byte[]? certificate, AcmeError? error)> IssueCertificate(string csr, CancellationToken cancellationToken);
