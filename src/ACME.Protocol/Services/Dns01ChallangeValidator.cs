bf4d70b5de1190537923743d6366a4cb28050b3d
Thomas Glatzer <glatzert@outlook.de>
2020-07-02 17:30:56 +0200

    Calculate DNS-01 expected content

--- src/ACME.Protocol/Services/Dns01ChallangeValidator.cs
+++ src/ACME.Protocol/Services/Dns01ChallangeValidator.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using TGIT.ACME.Protocol.Model;
@@ -9,7 +12,16 @@ namespace TGIT.ACME.Protocol.Services
     {
         protected override string GetExpectedContent(Challenge challenge, Account account)
         {
-            throw new NotImplementedException();
+            using var sha256 = SHA256.Create();
+
+            var thumbprintBytes = account.Jwk.SecurityKey.ComputeJwkThumbprint();
+            var thumbprint = Base64UrlEncoder.Encode(thumbprintBytes);
+
+            var keyAuthBytes = Encoding.UTF8.GetBytes($"{challenge.Token}.{thumbprint}");
+            var digestBytes = sha256.ComputeHash(keyAuthBytes);
+
+            var digest = Base64UrlEncoder.Encode(digestBytes);
+            return digest;
         }
 
         protected override Task<(string? Content, AcmeError? Error)> LoadChallengeResponseAsync(Challenge challenge, CancellationToken cancellationToken)
