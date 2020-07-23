d0b9736840fe3cd11687f61d649780fd74ae498d
Thomas Glatzer <glatzert@outlook.de>
2020-07-23 10:52:22 +0200

    Cleanup

--- src/ACME.Server/Filters/AcmeIndexLinkFilter.cs
+++ src/ACME.Server/Filters/AcmeIndexLinkFilter.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.Routing;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace TGIT.ACME.Server.Filters
 {
